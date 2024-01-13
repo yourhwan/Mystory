52b3d40d6f5c443d783ab5652add12f7d0d2a7a9
yourhwan <yunhwan9508@gmail.com>
2024-01-13 23:50:13 +0900

    if문만 사용하여 나의 하루를 프로젝트로 만들기 기본틀 완성

--- src/simulation/Ohday.java
+++ src/simulation/Ohday.java
@@ -10,20 +10,26 @@ public class Ohday {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("본격적으로 오윤환의 하루를 시작하기 원하신다면 숫자 1을 입력해 주세요. 2를 입력할 경우 해당 프로그램은 종료 됩니다.");
-
         int answer1 = sc.nextInt();
+
+        // 프로그램 시작 여부 질문
+        System.out.println("본격적으로 오윤환의 하루를 시작하기 원하신다면 숫자 1을 입력해 주세요. 2를 입력할 경우 해당 프로그램은 종료되며, 2회 이상 잘못 입력시 프로그램은 종료 됩니다.");
+
+        //answer1 = sc.nextInt();
 
         if (answer1 == 1) {
             System.out.println("본격 백수 오윤환의 하루를 시작합니다.");
 
-        } else if (answer1 == 2) {
+            // 이어서 스토리 진행
+
+        } else if (answer1 == 2) { // 프로그램 종료
             System.out.println("프로그램을 종료합니다. 다음에 만나요!");
-            sc.close();
 
-        } else {
-            System.out.println("⚠︎알림: 1 또는 2 중에서 선택해야 합니다. 올바른 답변을 입력 해주세요.");
+        } else { // 보기 이외의 입력값을 입력 했을 경우
+            System.out.println("⚠︎알림: 1 또는 2 중에서 선택해야 합니다. 프로그램을 종료합니다.");
+
         }
-
+        sc.close();
     }
-}
+
+}// public class
