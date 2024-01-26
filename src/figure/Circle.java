8ea2885157cead04a4113a27007e2d9308712d0f
yourhwan <yunhwan9508@gmail.com>
2024-01-27 00:38:06 +0900

    메인 메소드 생성

--- src/figure/Circle.java
+++ src/figure/Circle.java
@@ -1,5 +1,7 @@
 package figure;
 
 public class Circle {
+    public static void main(String[] args) {
 
+    }
 }
--- src/figure/Star.java
+++ src/figure/Star.java
@@ -1,5 +1,7 @@
 package figure;
 
 public class Star {
+    public static void main(String[] args) {
 
+    }
 }
